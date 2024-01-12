edff5de9412035d53679f919e8e0c9ab269f85c4
Marco Di Modica Elias <marcodimodica@protonmail.com>
2024-01-12 15:12:05 +0100

    a

--- Physics II - Bullet3D - Handout 05/ModulePlayer.cpp
+++ Physics II - Bullet3D - Handout 05/ModulePlayer.cpp
@@ -144,23 +144,23 @@ bool ModulePlayer::CleanUp()
 {
 	LOG("Unloading player");
 
-	//delete mushrooms and create new ones
-	for (int i = 0; i < 3; ++i) {
-		delete mushrooms[i];
-		mushrooms[i] = nullptr;
-	}
+	////delete mushrooms
+	//for (int i = 0; i < 3; ++i) {
+	//	delete mushrooms[i];
+	//	mushrooms[i] = nullptr;
+	//}
 
-	float orbDistance = 4.0f;
+	////volver a crear los mushrooms
+	//float orbDistance = 4.0f;
+	//for (int i = 0; i < 3; ++i) {
+	//	mushrooms[i] = new Cube(1, 1, 1);
 
-	for (int i = 0; i < 3; ++i) {
-		mushrooms[i] = new Cube(1, 1, 1);
-
-		object = App->physics->AddBody(Cube(1, 1, 1), 1);
-		object->SetPos(i * 23, 20, 10 + orbDistance);
-		mushrooms[i]->pbody = object;
-		App->physics->AddConstraintHinge(*vehicle, *object, vec3(0, 2, 0), vec3(0, 0, -orbDistance), vec3(0, 1, 0), vec3(0, 1, 0), true);
-		object->SetAsSensor(true);
-	}
+	//	object = App->physics->AddBody(Cube(1, 1, 1), 1);
+	//	object->SetPos(i * 23, 20, 10 + orbDistance);
+	//	mushrooms[i]->pbody = object;
+	//	App->physics->AddConstraintHinge(*vehicle, *object, vec3(0, 2, 0), vec3(0, 0, -orbDistance), vec3(0, 1, 0), vec3(0, 1, 0), true);
+	//	object->SetAsSensor(true);
+	//}
 
 	coins = 0;
 
@@ -168,8 +168,8 @@ bool ModulePlayer::CleanUp()
 	coinCollectSeconds = 0;
 	coinCollectMilliseconds = 0;
 
-	//resetting vehicle position
-	vehicle->SetPos(-12, 4, -12);
+	//resetting vehicle position and velocity
+	vehicle->SetPos(-26, 1.5f, 10);
 	vehicle->body->setAngularVelocity(btVector3(0, 0, 0));
 	vehicle->body->setLinearVelocity(btVector3(0, 0, 0));
 
